f8ccdac1a039ad934b0c5b114099c0f6c96cdc30
Puneet Tiwari <puneet.tewari@gmail.com>
2019-09-13 02:59:08 +0530

    procedure change

--- app/manager/procedure.js
+++ app/manager/procedure.js
@@ -4,7 +4,7 @@ const { database, username, host } = require('../config').SQL;
 async function update_color_range() {
     await sequelize.query(`DROP PROCEDURE IF EXISTS ${database}.\`Update_Color_Range\`;`);
     sequelize.query(`
-            CREATE DEFINER=\`${username}\`@\`${host}\` PROCEDURE ${database}.\`Update_Color_Range\`()
+            CREATE DEFINER=\`${username}\`@\`%\` PROCEDURE ${database}.\`Update_Color_Range\`()
             BEGIN
 
             DECLARE c_done BOOLEAN DEFAULT FALSE;
@@ -16,9 +16,9 @@ async function update_color_range() {
 
             DECLARE base_percent_cursor CURSOR FOR
             SELECT base_percent, color_hex FROM ${database}.Priority_Range WHERE is_active = 1;
-            
-            SET SQL_SAFE_UPDATES = 0;	
-            
+
+            SET SQL_SAFE_UPDATES = 0;
+
             BEGIN
             UPDATE ${database}.Claim_Summary cs,
                 (SELECT
@@ -32,8 +32,8 @@ async function update_color_range() {
                     cs.is_active = 1 AND
                     cs.claim_id = rs.claim_id ;
             END;
-            
-            
+
+
             BEGIN
             DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_done = TRUE;
             SELECT count(*) FROM ${database}.Claim_Summary cs
@@ -49,25 +49,25 @@ async function update_color_range() {
 
             OPEN base_percent_cursor;
                 get_base_percent_value: LOOP
-                
+
                 FETCH base_percent_cursor INTO v_base_percent,v_color_hex;
                 IF c_done THEN
 					LEAVE get_base_percent_value;
 				END IF;
                 SET @v_limit_start = @v_limit_start + @v_limit_end ;
                 SET @v_limit_end  =  round( (v_summary_count * v_base_percent) / 100);
-                
+
                 SET @sql = CONCAT('UPDATE Claim_Summary csp, (SELECT claim_id FROM ${database}.Claim_Summary cs LEFT JOIN ${database}.Status s on cs.status = s.status_code and cs.is_active =1 and s.is_active=1 where (s.mark_processed=0 or cs.status is null) order by cs.priority_score desc limit ?, ? ) rt SET csp.color_hex = ', "'", v_color_hex, "'", ' where rt.claim_id = csp.claim_id ');
-            
-                
+
+
                 PREPARE stmt FROM @sql;
                 EXECUTE stmt USING @v_limit_start, @v_limit_end;
                 DEALLOCATE PREPARE stmt;
-                
+
                 END LOOP get_base_percent_value;
             CLOSE base_percent_cursor;
             END;
-                    
+
             SET SQL_SAFE_UPDATES = 1;
             END;`)
         .then(function () {
